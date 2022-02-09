ce862aab947ef30654a406afa4ea30c61adfee46
harshraj172 <64323122+harshraj172@users.noreply.github.com>
2022-02-09 20:59:23 +0530

    Update torchutils.py

--- utils/torchutils.py
+++ utils/torchutils.py
@@ -35,3 +35,32 @@ def dumpModelSize(model, details=True):
 
 	print(f"total params: {total}, ", end='')
 	print(f"trainable params: {sum(p.numel() for p in model.parameters() if p.requires_grad)}")
+
+def downloadVOC(save_path=args.save_path, year=args.year, download=args.download):
+    """downloads the PascalVOC Dataset"""
+    datasets.VOCDetection(root=save_path, year=year, download=download, transform=transforms.ToTensor())
+
+def transformIMG(imgsize=args.imgsize, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]):
+    """Resize the raw image, Normalize it"""
+    tsfm = transforms.Compose([
+      transforms.Resize([imgsize, imgsize]),
+      transforms.ToTensor(),
+      transforms.Normalize(mean, std),
+    ])
+    return tsfm
+
+def SampleFromData(img_folder_path, n:int):
+    """Sample img path from the full list of images"""
+    imgFile_names = []
+    for file_ in os.listdir(img_folder_path):
+    imgFile_names.append(file_)
+
+    imgFile_names.sort()
+
+    # return random.sample(imgFile_names, n)
+    return imgFile_names[:n]
+
+def readURL(url):
+    resp = requests.get(url)
+    data = json.loads(resp.text)
+    return data
